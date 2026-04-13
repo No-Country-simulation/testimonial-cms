d6e00223a06511cd8f087f41384f54f6eb0b1ce2
testimonial-cms-test <jfcastropalma@gmail.com>
2026-04-12 22:16:13 -0400

    reparacion nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,12 +58,12 @@ export default async function Navbar() {
               <LogoutButton />
             </>
           ) : (
-            <Links
+            <Link
               href="/login?mode=login"
               className="bg-indigo-600 text-white text-sm px-4 py-2 rounded-lg hover:bg-indigo-700 transition-colors"
             >
               Iniciar sesión
-            </Links>
+            </Link>
           )}
         </div>
       </div>
